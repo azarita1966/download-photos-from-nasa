fa5749fa055c92d2300251915edaf2931a025c8d
Anna Azarova <86615443+azarita1966@users.noreply.github.com>
2023-02-13 14:45:36 +0100

    first download

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # download-photos-from-nasa
- Downloads photos from NASA EPIC to your computer
+
+## Downloads photos from NASA EPIC to your computer
+
+###download_epic_pics()
+
+
+
+Creates a list of images, available from the NASA epic site: 
+
+`https://api.nasa.gov/EPIC/api/natural?api_key=DEMO_KEY`
+
+
+Saves them in a list 'epic_images'
+
+
+###Algorithm:
+
+1. Create a list 'epic_images'.
+2. Save current date and time in a varaible 'now'.
+3. Using the names of images, add .png extensions.
+4. Create a list urls, where the name of the png file is a part of url.
+5. Create folder nasa/ in your computer.
+6. Save each image in a separate file in a nasa/ folder.
---
+++ download_photos_from_nasa.py
@@ -0,0 +1,36 @@
+#Download photos from NASA
+
+import requests
+import os
+import pathlib
+import datetime
+from urllib.parse import urlparse
+
+epic_images =[]
+def download_epic_pics():
+    epic_url = "https://api.nasa.gov/EPIC/api/natural?api_key=DEMO_KEY"
+    response = requests.get(epic_url)
+    response.raise_for_status()
+    response_json = response.json()
+    for record in response_json:
+        epic_images.append(record['image'])
+
+download_epic_pics()
+
+now = datetime.datetime.now()
+
+epic_urls =[]
+for image in epic_images:
+    picture =f'{image}.png'
+    new_path = "/".join(['https://api.nasa.gov/EPIC/archive/natural',str(now.year),str(now.month),str(now.day),'png',picture])
+    print(new_path)
+    new_url = f'{new_path}?api_key=DEMO_KEY'
+    epic_urls.append(new_url)
+print(epic_urls)
+
+for i, url in enumerate(epic_urls):
+    filepath = 'nasa/'
+    resp = requests.get(url)
+    filename =(f'{filepath}/epic_{i}')
+    with open(filename, 'wb') as file:
+        file.write(resp.content)
