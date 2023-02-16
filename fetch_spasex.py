0a6bfc625d95d1b7ab53096841a0d0e2d49b75ec
Anna Azarova <86615443+azarita1966@users.noreply.github.com>
2023-02-16 19:18:55 +0100

    Update fetch_spasex.py

--- fetch_spasex.py
+++ fetch_spasex.py
@@ -11,7 +11,9 @@ parser.parse_args()
 parser.add_argument('--launch_id',
                     help = 'print ID of the launch',
                     default = ['5eb87d47ffd86e000604b38a'])
-
+parser.add_argument('--series',
+                    help = 'print series of the launch',
+                    default = int(26))
 args = parser.parse_args()
 
 def fetch_spacex_launch(file_path):
@@ -19,13 +21,13 @@ def fetch_spacex_launch(file_path):
     payload = {'id':args.launch_id}
     response = requests.get(url, json=payload)
     response.raise_for_status()
-    resp =response.json()[31]['links']['flickr']['original']
+    resp =response.json()[args.series]['links']['flickr']['original']
     for number, link in enumerate(resp):
         photo_link = requests.get(link)
         filename =(f'{file_path}/spacex_{number}.jpeg')
         with open(filename, 'wb') as file:
             file.write(photo_link.content)
 
-if __name__ is '__main__'
-fetch_spacex_launch('new_spacex')
+if __name__ == '__main__':
+    fetch_spacex_launch('new_spacex')
 
