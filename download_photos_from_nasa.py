7da143d1e2290c1753718bc1a65ef97093211783
Anna Azarova <86615443+azarita1966@users.noreply.github.com>
2023-02-17 11:23:52 +0100

    Update download_photos_from_nasa.py

--- download_photos_from_nasa.py
+++ download_photos_from_nasa.py
@@ -6,32 +6,33 @@ from urllib.parse import urlparse
 
 epic_images =[]
 def download_epic_pics():
-    epic_url = "https://api.nasa.gov/EPIC/api/natural?api_key=DEMO_KEY"
-    response = requests.get(epic_url)
+    epic_url = "https://api.nasa.gov/EPIC/api/natural"
+    headers = {
+        "api_key": 'DEMO_KEY'}
+    response = requests.get(epic_url, headers=headers)
     response.raise_for_status()
     resp = response.json()
     for image in resp:
         epic_images = [image['image'] for image in resp]
-    return(epic_images)
+    return epic_images
 
 def main():
-    epic1 = download_epic_pics()
+    epic_images = download_epic_pics()
     now = datetime.datetime.now()
-    download_epic_pics()
     epic_urls =[]
-    for image in epic1:
+    for image in epic_images:
         picture =f'{image}.png'
         new_url = "/".join(['https://api.nasa.gov/EPIC/archive/natural',str(now.year),'02','15','png',picture])
         epic_urls.append(new_url)
 
     for number, url in enumerate(epic_urls):
-        filepath = 'nasa/'
+        directory = 'nasa/'
         headers = {
-                    "api_key": 'DEMO_KEY'}
+            "api_key": 'DEMO_KEY'}
         resp = requests.get(url, headers = headers)
         response.raise_for_status()
-        filename =(f'{filepath}/epic_{number}')
-        with open(filename, 'wb') as file:
+        filepath =(f'{directory}/epic_{number}')
+        with open(filepath, 'wb') as file:
             file.write(resp.content)
 
 if __name__ == '__main__':
